b28f08c18196ba2afcb91da14831a1d2fd39a4c1
elijahcox <elijah.thomas.cox@gmail.com>
2021-10-06 17:14:21 -0500

    begin main function, add headless opt

--- files/main.py
+++ files/main.py
@@ -1,5 +1,15 @@
 #main file used to take in input
 import locators, functions
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
 
-cdriver = webdriver.Chrome('../chromedriver')
+opt = Options()
+opt.headless = True
+cdriver = webdriver.Chrome('../chromedriver', options = opt)
+arglist = sys.argv
+arglen = len(arglist)
+
+
+def __main__():
+    #do main operations
