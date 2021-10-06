2a88dbe1a5c4723ce2b17d8799855fef52c148c0
elijahcox <elijah.thomas.cox@gmail.com>
2021-10-06 16:59:46 -0500

    add chromedriver location, dependencies

--- files/locators.py
+++ files/locators.py
@@ -1,1 +1,8 @@
 #class file of locators for expidition of automation
+from selenium import webdriver
+
+
+class AA_locators:
+    deptarture = ()
+    arrival = ()
+    one_way = ()
--- files/main.py
+++ files/main.py
@@ -1,2 +1,5 @@
 #main file used to take in input
 import locators, functions
+from selenium import webdriver
+
+cdriver = webdriver.Chrome('../chromedriver')
