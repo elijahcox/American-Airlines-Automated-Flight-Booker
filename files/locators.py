c522eadc165b97c92833d531301eb444d717c53f
elijahcox <elijah.thomas.cox@gmail.com>
2021-10-07 17:14:22 -0500

    update locators for advanced search page

--- files/locators.py
+++ files/locators.py
@@ -2,12 +2,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 class SW_locators:
-    class SW_Flight_Search_Bar:
-        Departure_Location = (By.ID, "LandingAirBookingSearchForm_originationAirportCode")
-        Arrival_Location = (By.ID, "LandingAirBookingSearchForm_destinationAirportCode")
-        Departure_Date = (By.ID, "LandingAirBookingSearchForm_departureDate")
-        Return_Date = (By.ID,"LandingAirBookingSearchForm_returnDate")
+    class SW_Advanced_Flight_Search:
+        Departure_Location = (By.ID, "originationAirportCode")
+        Arrival_Location = (By.ID, "destinationAirportCode")
+        Departure_Date = (By.ID, "departureDate")
+        Return_Date = (By.ID,"returnDate")
         One_Way_Checkbox = ()
         Round_Trip_Checkbox = ()
--- files/main.py
+++ files/main.py
@@ -1,23 +1,19 @@
-#main file used to take in input
 import locators, functions
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import sys, time
 
-opt = Options()
-opt.headless = True
-cdriver = webdriver.Chrome('../chromedriver', options = opt)
+southwest_start_url = "https://www.southwest.com/air/booking/index.html?int=HOME-BOOKING-WIDGET-ADVANCED-AIR"
+
+options = Options()
+options.headless = True
+cdriver = webdriver.Chrome('../chromedriver', options = options)
 arglist = sys.argv
-arglen = len(arglist)
 
 def print_usage():
-    print("")
-    return
-
-
-def  main():
-    if arglen < 2:
-        print_usage()
+    print("Usage: ")
+    return    
 
 if __name__ == "__main__":
-    main()
+    cdriver.get(southwest_start_url)
+    
