f28530982ab9cba05cb22da7d110df737d88b7a7
elijahcox <elijah.thomas.cox@gmail.com>
2021-10-07 19:10:11 -0500

    add selenium workflor for departure, arrival, and departure date

--- files/locators.py
+++ files/locators.py
@@ -2,11 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class SW_locators:
-    class SW_Advanced_Flight_Search:
-        Departure_Location = (By.ID, "originationAirportCode")
-        Arrival_Location = (By.ID, "destinationAirportCode")
-        Departure_Date = (By.ID, "departureDate")
-        Return_Date = (By.ID,"returnDate")
-        Round_Trip_Checkbox = ()
-        One_Way_Checkbox = ()
-        Search_Button = (By.ID, "form-mixin--submit-button")
+    Departure_Location = (By.ID, "originationAirportCode")
+    Arrival_Location = (By.ID, "destinationAirportCode")
+    Departure_Date = (By.ID, "departureDate")
+    Return_Date = (By.ID,"returnDate")
+    Round_Trip_Checkbox = (By.XPATH, "//span[contains(.,'Round trip')]")
+    One_Way_Checkbox = (By.XPATH, "//li[2]/label/input") #need better locator
+    Search_Button = (By.ID, "form-mixin--submit-button")
--- files/main.py
+++ files/main.py
@@ -1,24 +1,75 @@
-import locators, functions
+from locators import SW_locators
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import sys, time
-import argparse
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+import sys, argparse, functions
+from time import sleep
+from datetime import datetime
 
 southwest_start_url = "https://www.southwest.com/air/booking/index.html?int=HOME-BOOKING-WIDGET-ADVANCED-AIR"
 
 options = Options()
-options.headless = True
+options.headless = False
+options.add_argument('--disable-blink-features=AutomationControlled')
 cdriver = webdriver.Chrome('../chromedriver', options = options)
-arglist = sys.argv
+cdriver.maximize_window()
+cdriver_wait = WebDriverWait(cdriver, 10)
 
-def print_usage():
-    print("Usage: ")
-    return    
+parser = argparse.ArgumentParser(description='This is a CL tool to automate the booking of flights through Southwest Airlines.')
+parser.add_argument("DEPART", help = "Enter the three letter airport code for departure location",type=str)
+parser.add_argument("ARRIVAL", help = "Enter the three letter airport code for arrival location",type=str)
+parser.add_argument("DEPART_DATE", help = "Enter the departure date in the format MM/DD",type=str)
+#parser.add_argument("ARRIVAL DATE", help = "Enter the three letter airport code for arrival location")
+args = parser.parse_args()
+
+
+def type_slow(to_enter,element):
+    for i in range(0,len(to_enter)):
+        element.send_keys(to_enter[i])
+        sleep(.15)
+
+
 
 if __name__ == "__main__":
     #1. Parse CL input for commands
+    depart = args.DEPART
+    arrival = args.ARRIVAL
+    dept_Date = args.DEPART_DATE
+
     #2. Sanitize inputs such as Depart and Arrival Code, ETC.
-    #3. Open the start url and input data
-    #4 click search
+    assert(len(depart) == len(arrival) == 3)
+    assert(len(dept_Date) == 5) #cur_time = datetime.today().strftime('%m-%d') - TODO: implement departure date check, within 5 months of cur time
+
+    #3. Open the start url
     cdriver.get(southwest_start_url)
+
+    #click one way
+    cdriver_wait.until(EC.element_to_be_clickable(SW_locators.One_Way_Checkbox))
+    element = cdriver.find_element_by_xpath(SW_locators.One_Way_Checkbox[1])
+    element.click()
+
+    #enter depart airport code
+    cdriver_wait.until(EC.element_to_be_clickable(SW_locators.Departure_Location))
+    element = cdriver.find_element_by_id(SW_locators.Departure_Location[1])
+    type_slow(depart,element)
+    element = cdriver.find_element_by_xpath("//button[contains(.,\"" + depart + "\")]").click()
+
+    #enter arrival airport code
+    cdriver_wait.until(EC.element_to_be_clickable(SW_locators.Arrival_Location))
+    element = cdriver.find_element_by_id(SW_locators.Arrival_Location[1])
+    type_slow(arrival,element)
+    element = cdriver.find_element_by_xpath("//button[contains(.,\"" + arrival + "\")]").click()
+
+    #enter depart date
+    cdriver_wait.until(EC.element_to_be_clickable(SW_locators.Departure_Date))
+    element = cdriver.find_element_by_id(SW_locators.Departure_Date[1])
+    element.click()
+    element.send_keys(Keys.DELETE + dept_Date + Keys.ESCAPE)
+
+    #4click search
+    element = cdriver.find_element_by_id(SW_locators.Search_Button[1]).click()
+    sleep(5)
     
+    cdriver.close()
